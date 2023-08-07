0226cc27f06305a2109ee96ac3ceff8f735c6a7d
Sycrusian <sycrus@protonmail.com>
2023-08-07 17:12:04 -0300

    Atividade - Criando Filtro de Dados

--- script/script.js
+++ script/script.js
@@ -2,30 +2,30 @@
 
 //1) Crie uma função para filtrar os hotéis que estão localizados no Brasil
 function filterCountry(array) {
-  return;
+  return array.filter(hotel => hotel.country === "Brasil");
 }
 console.log(filterCountry(data));
 
 // 2) Crie uma função para filtrar os hotéis que possuem o preço igual ou maior que R$200,00.
 function filterPrice(array) {
-  return;
+  return array.filter(hotel => hotel.price >= 200);
 }
 console.log(filterPrice(data));
 
 //3) Crie uma função para filtrar os hotéis que estão abertos nesse momento (isOpen)
 function filterIsOpen(array) {
-  return;
+  return array.filter(hotel => hotel.isOpen);
 }
 console.log(filterIsOpen(data));
 
 //DESAFIO
 //4) Crie uma função que filtre apenas o hotel com o nome “Copacabana Palace”. Após isso, crie outra função para verificar as datas disponíveis para agendamento nesse hotel (isAvaliable).
 function filterHotelName(array) {
-  return;
+  return array.find(hotel => hotel.name === "Copacabana Palace");
 }
 
 function filterToBook() {
   const hotel = filterHotelName(data);
-  return;
+  return hotel.toBook.filter(booking => booking.isAvaliable);
 }
 console.log(filterToBook());
